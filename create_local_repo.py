a85ed3dd7274bde58fb9c3fd583b31904ee582ce
apoorva-mk <apoorva.mk99@gmail.com>
2020-04-11 20:46:29 +0530

    very basic scipts

---
+++ create_local_repo.py
@@ -0,0 +1,17 @@
+import os
+
+
+repo_name = "test"
+
+
+# Make the git directory
+if os.system('mkdir '+repo_name)==0:
+    os.chdir(repo_name)
+
+    # Initialize a new bare repo
+    os.system('git init')
+    os.system('touch Jenkinsfile') 
+
+        
+else:
+    print("Repo already exists")
---
+++ jenkins-script.py
@@ -0,0 +1,14 @@
+import jenkins
+
+
+#TODO: Use API token instead of password 
+server = jenkins.Jenkins('http://localhost:8080', username='Apoorva', password='123456')
+user = server.get_whoami()
+version = server.get_version()
+print('Hello %s from Jenkins %s' % (user['fullName'], version))
+
+
+#Creating a job
+server.create_job('empty', jenkins.EMPTY_CONFIG_XML)
+jobs = server.get_jobs()
+print(jobs)

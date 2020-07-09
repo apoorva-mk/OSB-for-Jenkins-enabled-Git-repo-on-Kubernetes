7d16080dcb2703fb1235b361164cd508f124e76f
apoorva-mk <apoorva.mk99@gmail.com>
2020-07-09 05:49:54 +0530

    issue of git config fixed temporarily, tbd: find a cleaner way to configure

--- jenkins_script.py
+++ jenkins_script.py
@@ -7,7 +7,7 @@ JENKINS_JOB_DELETION_SUCCESS = "Jenkins job successfully deleted"
 JENKINS_AUTH_FAILURE = "Unauthorised access to Jenkins server"
 
 
-def create_new_jenkins_job(jenkins_host, username, password, job_name):
+def create_new_jenkins_job(jenkins_host, username, password, job_name, github_repo_url):
     """
         Creates a new jenkins job, associated with a github repository
         Returns:
@@ -23,8 +23,9 @@ def create_new_jenkins_job(jenkins_host, username, password, job_name):
         return 401, JENKINS_AUTH_FAILURE
 
     """ Create associated jenkins job """
+    GITHUB_CONFIG_XML = get_job_xml_config(github_repo_url)
     try:
-        server.create_job(job_name, jenkins.EMPTY_CONFIG_XML)
+        server.create_job(job_name, GITHUB_CONFIG_XML)
         return 201, JENKINS_JOB_CREATION_SUCCESS
     except:
         return 400, JENKINS_JOB_CREATION_FAILURE
@@ -61,6 +62,55 @@ def get_jenkins_job_name(auth_token, repo_name):
     github_user_name = g.get_user().login
     job_name = github_user_name+"."+repo_name
     return job_name
+
+def get_job_xml_config(github_repo_url):
+    p1 = "<?xml version='1.1' encoding='UTF-8'?>\
+            <project>\
+            <description></description>\
+            <keepDependencies>false</keepDependencies>\
+            <properties>\
+                <com.coravy.hudson.plugins.github.GithubProjectProperty plugin=\"github@1.29.5\">\
+                <projectUrl>"
+    p2 = "</projectUrl>\
+            <displayName></displayName>\
+            </com.coravy.hudson.plugins.github.GithubProjectProperty>\
+        </properties>\
+        <scm class=\"hudson.plugins.git.GitSCM\" plugin=\"git@4.2.2\">\
+            <configVersion>2</configVersion>\
+            <userRemoteConfigs>\
+            <hudson.plugins.git.UserRemoteConfig>\
+                <url>"
+    p3 = "</url>\
+        </hudson.plugins.git.UserRemoteConfig>\
+        </userRemoteConfigs>\
+        <branches>\
+        <hudson.plugins.git.BranchSpec>\
+            <name>*/master</name>\
+        </hudson.plugins.git.BranchSpec>\
+        </branches>\
+        <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>\
+        <submoduleCfg class=\"list\"/>\
+        <extensions/>\
+    </scm>\
+    <canRoam>true</canRoam>\
+    <disabled>false</disabled>\
+    <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>\
+    <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>\
+    <triggers>\
+        <com.cloudbees.jenkins.GitHubPushTrigger plugin=\"github@1.29.5\">\
+        <spec></spec>\
+        </com.cloudbees.jenkins.GitHubPushTrigger>\
+    </triggers>\
+    <concurrentBuild>false</concurrentBuild>\
+    <builders/>\
+    <publishers/>\
+    <buildWrappers/>\
+    </project>"
+
+    return p1+github_repo_url+p2+github_repo_url+p3
+
+
+create_new_jenkins_job('http://localhost:9090','apoorva','123456',"ex2", "https://github.com/apoorva-mk/trial1")
 
 
 """ Some tidbits of code (for authors reference -- kindly ignore)
